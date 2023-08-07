b54c6cef44584c54fc922ba6f23ed1d1c2003c8c
Sravan3618 <141633781+Sravan3618@users.noreply.github.com>
2023-08-07 19:40:56 +0530

    Add files via upload

---
+++ Shopping.js
@@ -0,0 +1,12 @@
+
+function dynamicfirstboxview(){
+
+    const first=document.getElementById("firstboxbackground")
+    first.style.backgroundColor="aquamarine";
+}
+
+function dynamicthirdboxview(){
+    const third=document.getElementById("thirdboxbackground")
+    third.style.backgroundColor="aquamarine";
+
+}
